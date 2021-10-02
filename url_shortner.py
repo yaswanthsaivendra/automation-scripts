c59d28e2c287375908f3ca4221ea51d8e2ef23bd
Devesh Pal <competitive.schooling.info@gmail.com>
2021-10-02 23:26:35 +0530

    Update url_shortner.py

--- url_shortner.py
+++ url_shortner.py
@@ -6,13 +6,13 @@ import requests
 BASE_URI = "https://da.gd/s?url="
 
 # ask input from User.
-URL = input("- Enter The Link, you want to shorten...")
+URL = input("- Enter The Link, you want to shorten...\n---> ")
 REQ_URI = BASE_URI + URL
 
 # make a request to dagd
 req = requests.get(REQ_URI).text
 
 # print output
-print("--- Url Shortened ---\n\n")
-print(f"- Given Url : {URL}\n")
+print("--- Url Shortened ---\n")
+print(f"- Given Url : {URL}")
 print(f"- Shortened Url : {req}")
